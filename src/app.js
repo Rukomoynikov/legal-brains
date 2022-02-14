8b1352d93f3142ffa729d153b48ce77c2dd31941
Maksim Rukomoynikov <maksim@rukomoynikov.ru>
2022-02-14 11:30:55 +0300

    Smooth scrolling

--- src/app.js
+++ src/app.js
@@ -75,3 +75,20 @@ var flyGradiend = {
 };
 
 flyGradiend.init();
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("[href='#team']").addEventListener("click", function(event) {
+    event.preventDefault()
+    window.scrollTo({
+      top: offset(document.querySelector("#team")).top,
+      behavior: 'smooth'
+    });
+  })
+})
